c4aac60ae9c920a1e652c28b7e4174e4f595e835
cjrequena <requenacj@gmail.com>
2021-09-22 11:14:04 +0200

    Added flyway

---
+++ src/main/java/com/cjrequena/sample/configuration/DatabaseConfiguration.java
@@ -0,0 +1,69 @@
+package com.cjrequena.sample.configuration;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ *
+ * @author cjrequena
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+  entityManagerFactoryRef = "entityManagerFactory",
+  transactionManagerRef = "transactionManager",
+  basePackages = {"com.cjrequena.sample.db.repository"}
+)
+public class DatabaseConfiguration {
+
+  @Bean(name = "dataSource", destroyMethod = "")
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).build();
+  }
+
+  /**
+   *
+   * @param builder
+   * @param dataSource
+   * @return
+   */
+  @Primary
+  @Bean("entityManagerFactory")
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder, @Qualifier("dataSource") DataSource dataSource) {
+    return builder
+      .dataSource(dataSource)
+      .packages("com.cjrequena.sample.domain")
+      .persistenceUnit("cqrs")
+      .build();
+  }
+
+  /**
+   *
+   * @param entityManagerFactory
+   * @return
+   */
+  @Primary
+  @Bean("transactionManager")
+  public PlatformTransactionManager transactionManager(@Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+    return new JpaTransactionManager(entityManagerFactory);
+  }
+}
+
+
--- src/main/java/com/cjrequena/sample/domain/BankAccountEntity.java
+++ src/main/java/com/cjrequena/sample/domain/BankAccountEntity.java
@@ -3,10 +3,15 @@ package com.cjrequena.sample.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.UUID;
 
 /**
@@ -19,9 +24,19 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "bank_account")
 public class BankAccountEntity {
   @Id
+  @Column(name = "account_id")
   private UUID accountId;
+
+  @Column(name = "owner")
   private String owner;
+
+  @Column(name = "balance")
   private BigDecimal balance;
+
+  @Column(name = "creation_date")
+  @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+  private LocalDate creationDate;
 }
